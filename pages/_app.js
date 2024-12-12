d0ec6d1ccbdc6b8fed3b31e0f865b423d5acc705
Avani <avanicious@gmail.com>
2024-12-13 01:37:11 +0530

    updated _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 // pages/_app.js
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const customTheme = extendTheme({})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
